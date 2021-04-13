7d5fab316a05c54c1b3d64e50ad77832b1dd70b2
ngongangdirane <82378020+ngongangdirane@users.noreply.github.com>
2021-04-13 03:40:14 -0700

    Add files via upload

---
+++ calcu/calcu/Form1.Designer.vb
@@ -0,0 +1,246 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.textRoot1 = New System.Windows.Forms.TextBox()
+        Me.compute = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.textBox1 = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.textBox2 = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.textBox3 = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.textRoot2 = New System.Windows.Forms.TextBox()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.clear = New System.Windows.Forms.Button()
+        Me.exitIt = New System.Windows.Forms.Button()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(107, 114)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(76, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Enter value a :"
+        '
+        'textRoot1
+        '
+        Me.textRoot1.Location = New System.Drawing.Point(437, 134)
+        Me.textRoot1.Name = "textRoot1"
+        Me.textRoot1.Size = New System.Drawing.Size(100, 20)
+        Me.textRoot1.TabIndex = 1
+        '
+        'compute
+        '
+        Me.compute.Location = New System.Drawing.Point(168, 297)
+        Me.compute.Name = "compute"
+        Me.compute.Size = New System.Drawing.Size(121, 35)
+        Me.compute.TabIndex = 2
+        Me.compute.Text = "Compute"
+        Me.compute.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(107, 144)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(76, 13)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "Enter value b :"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(107, 178)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(76, 13)
+        Me.Label3.TabIndex = 0
+        Me.Label3.Text = "Enter value c :"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(-154, 221)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(39, 13)
+        Me.Label4.TabIndex = 0
+        Me.Label4.Text = "Label1"
+        '
+        'textBox1
+        '
+        Me.textBox1.Location = New System.Drawing.Point(189, 111)
+        Me.textBox1.Name = "textBox1"
+        Me.textBox1.Size = New System.Drawing.Size(100, 20)
+        Me.textBox1.TabIndex = 1
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(-154, 251)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(39, 13)
+        Me.Label5.TabIndex = 0
+        Me.Label5.Text = "Label1"
+        '
+        'textBox2
+        '
+        Me.textBox2.Location = New System.Drawing.Point(189, 141)
+        Me.textBox2.Name = "textBox2"
+        Me.textBox2.Size = New System.Drawing.Size(100, 20)
+        Me.textBox2.TabIndex = 1
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(-154, 284)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(39, 13)
+        Me.Label6.TabIndex = 0
+        Me.Label6.Text = "Label1"
+        '
+        'textBox3
+        '
+        Me.textBox3.Location = New System.Drawing.Point(189, 174)
+        Me.textBox3.Name = "textBox3"
+        Me.textBox3.Size = New System.Drawing.Size(100, 20)
+        Me.textBox3.TabIndex = 1
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(434, 100)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(42, 13)
+        Me.Label7.TabIndex = 0
+        Me.Label7.Text = "Results"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(365, 141)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(39, 13)
+        Me.Label8.TabIndex = 0
+        Me.Label8.Text = "Root 1"
+        '
+        'textRoot2
+        '
+        Me.textRoot2.Location = New System.Drawing.Point(437, 178)
+        Me.textRoot2.Name = "textRoot2"
+        Me.textRoot2.Size = New System.Drawing.Size(100, 20)
+        Me.textRoot2.TabIndex = 1
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(365, 181)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(39, 13)
+        Me.Label9.TabIndex = 0
+        Me.Label9.Text = "Root 2"
+        '
+        'clear
+        '
+        Me.clear.Location = New System.Drawing.Point(321, 297)
+        Me.clear.Name = "clear"
+        Me.clear.Size = New System.Drawing.Size(83, 35)
+        Me.clear.TabIndex = 2
+        Me.clear.Text = "Clear All"
+        Me.clear.UseVisualStyleBackColor = True
+        '
+        'exitIt
+        '
+        Me.exitIt.Location = New System.Drawing.Point(469, 297)
+        Me.exitIt.Name = "exitIt"
+        Me.exitIt.Size = New System.Drawing.Size(83, 35)
+        Me.exitIt.TabIndex = 2
+        Me.exitIt.Text = "Exit "
+        Me.exitIt.UseVisualStyleBackColor = True
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Font = New System.Drawing.Font("Microsoft Sans Serif", 26.0!)
+        Me.Label10.Location = New System.Drawing.Point(117, 33)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(435, 39)
+        Me.Label10.TabIndex = 0
+        Me.Label10.Text = "Super Quadratic Calculator"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.exitIt)
+        Me.Controls.Add(Me.clear)
+        Me.Controls.Add(Me.compute)
+        Me.Controls.Add(Me.textRoot2)
+        Me.Controls.Add(Me.textBox3)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.textBox2)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.textBox1)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.textRoot1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Quadratic"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents textRoot1 As TextBox
+    Friend WithEvents compute As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents textBox1 As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents textBox2 As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents textBox3 As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents textRoot2 As TextBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents clear As Button
+    Friend WithEvents exitIt As Button
+    Friend WithEvents Label10 As Label
+End Class
---
+++ calcu/calcu/Form1.vb
@@ -0,0 +1,40 @@
+﻿Public Class Form1
+    Dim a As Integer
+    Dim b As Integer
+    Dim c As Integer
+    Dim d As Integer
+    Dim root1 As Single
+    Dim root2 As Single
+
+
+
+    Private Sub Buttonclickmethod(sender As Object, e As EventArgs) Handles compute.Click, clear.Click, exitIt.Click
+        Dim button As Button = CType(sender, Button)
+        If button.Name = "compute" Then
+            a = textBox1.Text
+            b = textBox2.Text
+            c = textBox3.Text
+            d = (b ^ 2) - (4 * a * c)
+            root1 = (-b + Math.Sqrt(d)) / (2 * a)
+            root2 = (-b + Math.Sqrt(d)) / (2 * a)
+            textRoot1.Text = root1
+            textRoot2.Text = root2
+        End If
+        If button.Name = "clear" Then
+            textRoot1.Text = ""
+            textRoot2.Text = ""
+            textBox1.Text = ""
+            textBox2.Text = ""
+            textBox3.Text = ""
+
+        End If
+        If button.Name = "exitIt" Then
+
+            Application.Exit()
+        End If
+    End Sub
+
+    Private Sub textBox1_TextChanged(sender As Object, e As EventArgs) Handles textBox1.TextChanged
+
+    End Sub
+End Class
